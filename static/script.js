f73f746f0baeb4d32ad5454660354520b08ebfd0
Amanteloca <117738244+Amanteloca@users.noreply.github.com>
2023-09-06 19:09:39 +0100

    Create script.js
    added script.js for landing page

---
+++ static/script.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.nav-links a');
+  const navbar = document.querySelector('.navbar');
+  const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+  const sections = document.querySelectorAll('section');
+
+  // Smooth scroll navigation
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+      if (targetSection) {
+        window.scrollTo({
+          top: targetSection.offsetTop - navbar.offsetHeight,
+          behavior: 'smooth',
+        });
+      }
+    });
+  });
+
+  // Highlight active navigation link
+  window.addEventListener('scroll', () => {
+    let currentSectionId = '';
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop - navbar.offsetHeight;
+      const sectionBottom = sectionTop + section.clientHeight;
+      if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+        currentSectionId = section.getAttribute('id');
+      }
+    });
+    navLinks.forEach((link) => {
+      const linkId = link.getAttribute('href').substring(1);
+      link.classList.toggle('active', linkId === currentSectionId);
+    });
+  });
+
+  // Mobile navigation toggle
+  mobileNavToggle.addEventListener('click', () => {
+    navbar.classList.toggle('show-mobile-nav');
+  });
+
+  // Close mobile navigation on link click
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      navbar.classList.remove('show-mobile-nav');
+    });
+  });
+
+  // Scroll reveal animations
+  ScrollReveal().reveal('.feature', {
+    delay: 200,
+    distance: '20px',
+    origin: 'bottom',
+  });
+
+  ScrollReveal().reveal('.team-member', {
+    delay: 200,
+    distance: '20px',
+    origin: 'bottom',
+  });
+
+  ScrollReveal().reveal('.cta-button', {
+    delay: 400,
+    distance: '20px',
+    origin: 'bottom',
+  });
+
+  // Custom JavaScript code can be added here to further enhance your landing page.
+});
