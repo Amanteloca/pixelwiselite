1b38081773356c37995b9d5d367df64d4f800df3
Amanteloca <117738244+Amanteloca@users.noreply.github.com>
2023-09-06 20:32:07 +0100

    Update pixelwiselite_image_compression.py
    added  some few lines

--- pixelwiselite_image_compression.py
+++ pixelwiselite_image_compression.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 import os
 import logging
 
@@ -6,23 +6,31 @@ def compress_and_save_image(input_path, output_folder, max_width, compression_qu
     try:
         # Open the image
         with Image.open(input_path) as image:
+            # Apply image enhancements (e.g., sharpening)
+            enhanced_image = apply_image_enhancements(image)
+
             # Calculate the new height to maintain aspect ratio
-            width, height = image.size
+            width, height = enhanced_image.size
             new_height = int(max_width * height / width)
-            
-            # Resize the image
-            resized_image = image.resize((max_width, new_height), Image.ANTIALIAS)
-            
+
+            # Resize the image using Lanczos resampling for better quality
+            resized_image = enhanced_image.resize((max_width, new_height), Image.LANCZOS)
+
             # Create the output path for the compressed image
             compressed_filename = f"compressed_{os.path.basename(input_path)}"
             compressed_path = os.path.join(output_folder, compressed_filename)
-            
-            # Save the compressed image with specified quality
-            resized_image.save(compressed_path, quality=compression_quality)
-            
+
+            # Save the compressed image in WebP format for efficient compression
+            resized_image.save(compressed_path, "WebP", quality=compression_quality)
+
             return compressed_filename  # Return the filename of the compressed image
     except FileNotFoundError:
         logging.error("Input image file not found.")
     except Exception as e:
         logging.error(f"Error during compression: {e}")
     return None
+
+def apply_image_enhancements(image):
+    # Apply advanced image enhancements (e.g., sharpening) using PIL's filters
+    enhanced_image = image.filter(ImageFilter.SHARPEN)
+    return enhanced_image
