5ba7dc63c727cb25afe55327f143bdfa9313897d
Amanteloca <117738244+Amanteloca@users.noreply.github.com>
2023-09-10 17:30:06 +0100

    Update script_for_compression.js
    spelling errors corrected

--- static/script_for_compression.js
+++ static/script_for_compression.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
     loadingIndicator.style.display = 'block';
 
     try {
-      const response = await fetch('/compression', {
+      const response = await fetch('/compress_image', {
         method: 'POST',
         body: formData,
       });
