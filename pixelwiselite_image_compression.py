bd498c1f1143a2efa846e390e9728c82225aa9f3
Amanteloca <117738244+Amanteloca@users.noreply.github.com>
2023-09-18 19:14:56 +0100

    Update pixelwiselite_image_compression.py
    updates

--- pixelwiselite_image_compression.py
+++ pixelwiselite_image_compression.py
@@ -1,45 +1,78 @@
-from flask import Flask, render_template, request, jsonify
+import os
 from PIL import Image
 import numpy as np
-import os
-import sys
 
-app = Flask(__name__)
-
-# Function to load an image from a path
 def load_image(path):
+    """ Load image from path. Return a numpy array """
     image = Image.open(path)
     return np.asarray(image) / 255
 
-# Function to perform K-means clustering for image compression
+def initialize_K_centroids(X, K):
+    """ Choose K points from X at random """
+    m = len(X)
+    return X[np.random.choice(m, K, replace=False), :]
+
+def find_closest_centroids(X, centroids):
+    m = len(X)
+    c = np.zeros(m)
+
+    for i in range(m):
+        # Find distances
+        distances = np.linalg.norm(X[i] - centroids, axis=1)
+
+        # Assign closest cluster to c[i]
+        c[i] = np.argmin(distances)
+
+    return c
+
+def compute_means(X, idx, K):
+    _, n = X.shape
+    centroids = np.zeros((K, n))
+    for k in range(K):
+        examples = X[np.where(idx == k)]
+        mean = [np.mean(column) for column in examples.T]
+        centroids[k] = mean
+    return centroids
+
 def find_k_means(X, K, max_iters=10):
-    # (Include your K-means code here)
+    centroids = initialize_K_centroids(X, K)
+    previous_centroids = centroids
+    for _ in range(max_iters):
+        idx = find_closest_centroids(X, centroids)
+        centroids = compute_means(X, idx, K)
+        if (previous_centroids==centroids).all():
+            # The centroids aren't moving anymore.
+            return centroids
+        else:
+            previous_centroids = centroids
 
-# Function to compress and save an image
-def compress_and_save_image(input_image_path, output_image_path, K=40, max_iters=20):
+    return centroids, idx
+
+def compress_and_save_image(input_image, quality=40):
     try:
-        assert os.path.isfile(input_image_path)
-    except AssertionError:
-        raise Exception('Image file not found.')
+        # Load the image
+        image = load_image(input_image)
+        w, h, d = image.shape
 
-    # Load the image
-    image = load_image(input_image_path)
-    w, h, d = image.shape
+        # Get the feature matrix X
+        X = image.reshape((w * h, d))
+        K = quality  # the number of colors in the image
 
-    # Get the feature matrix X
-    X = image.reshape((w * h, d))
+        # Get colors
+        colors, _ = find_k_means(X, K, max_iters=20)
 
-    # Get colors using K-means clustering
-    colors, _ = find_k_means(X, K, max_iters=max_iters)
+        # Indexes for color for each pixel
+        idx = find_closest_centroids(X, colors)
 
-    # Indexes for color for each pixel
-    idx = find_closest_centroids(X, colors)
+        # Reconstruct the image
+        idx = np.array(idx, dtype=np.uint8)
+        X_reconstructed = np.array(colors[idx, :] * 255, dtype=np.uint8).reshape((w, h, d))
+        compressed_image = Image.fromarray(X_reconstructed)
 
-    # Reconstruct the image
-    idx = np.array(idx, dtype=np.uint8)
-    X_reconstructed = np.array(colors[idx, :] * 255, dtype=np.uint8).reshape((w, h, d))
-    compressed_image = Image.fromarray(X_reconstructed)
+        # Save reconstructed image to disk
+        output_path = 'compressed_image.jpg'  # You can specify a different output path
+        compressed_image.save(output_path)
 
-    # Save reconstructed image to the specified path
-    compressed_image.save(output_image_path)
-
+        return output_path
+    except Exception as e:
+        return str(e)
